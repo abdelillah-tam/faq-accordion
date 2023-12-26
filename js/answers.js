c7587b68b8312443290222d63a556b06d858f73a
Abdelillah <atamossat@gmail.com>
2023-12-26 17:56:52 +0100

    Edited javascript file

--- js/answers.js
+++ js/answers.js
@@ -28,29 +28,3 @@ for (let i = 0; i < plus.length; i++) {
   plus[i].onclick = on;
   questions[i].onclick = on;
 }
-
-/*var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    var nextToContent = content.nextElementSibling;
-    console.log(nextToContent);
-    if (
-      content.style.display === "block" &&
-      nextToContent.style.display === "block"
-    ) {
-      content.style.display = "none";
-      nextToContent.style.display = "none";
-      content.style.maxHeight = null;
-      nextToContent.style.maxHeight = null;
-    } else {
-      content.style.display = "block";
-      nextToContent.style.display = "block";
-      content.style.maxHeight = content.scrollHeight + "px";
-      nextToContent.style.maxHeight = nextToContent.scrollHeight + "px";
-    }
-  });
-}*/
